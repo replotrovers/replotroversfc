7be88d12fb51a34259464b9948b1278eeadb6e9c
replotrovers <replotrovers@gmail.com>
2026-06-05 23:29:25 +0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const menuButton = document.getElementById("menuButton");
+const mobileNav = document.getElementById("mobileNav");
+
+menuButton.addEventListener("click", () => {
+
+    mobileNav.classList.toggle("show");
+
+});
